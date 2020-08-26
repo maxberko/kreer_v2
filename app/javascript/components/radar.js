aa0ee2774beff46069d083a7a23006438a6665c8
maxime berkowicz <maximeberkowicz@gmail.com>
2020-08-26 15:53:14 +0200

    graph working properly

--- app/javascript/components/radar.js
+++ app/javascript/components/radar.js
@@ -1,21 +1,24 @@
 const graph = () => {
-    if (document.getElementById('myChart')) {
-        
-    var ctx = document.getElementById('myChart').getContext('2d');
+    const canva = document.getElementById('myChart');
+    if (canva) { 
+    const labels = JSON.parse(canva.dataset.labels);
+    const meanValues = JSON.parse(canva.dataset.meanValues);
+    const candidateValues = JSON.parse(canva.dataset.candidateValues);
+    var ctx = canva.getContext('2d');
     var chart = new Chart(ctx, {
         // The type of chart we want to create
         type: 'radar',
         // The data for our dataset
         data: {
-            labels: ['January', 'March', 'May', 'June', 'July'],
+            labels: labels,
             datasets: [{
                 label: 'This candidate',
                 borderColor: 'rgb(38, 255, 6)',
-                data: [10, 8, 2, 10, 6],},
+                data: candidateValues,},
                 {
                 label: 'All candidates',
                 borderColor: 'rgb(255, 99, 132)',
-                data: [0, 5, 2, 3, 6]
+                data: meanValues
             }]
         },
         // Configuration options go here
